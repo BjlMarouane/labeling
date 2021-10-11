1533afd26fad2ee92baf11cfaddd427ff4d6676a
Marouane Benjillali <benm0131@uqar.ca>
2021-10-11 09:40:23 -0400

    link to remote repo

---
+++ main.js
@@ -0,0 +1,346 @@
+var user_id = "";
+var current_commit = "";
+var commits = [];
+var activities = "";
+var clusters = [];
+var errors = [];
+
+$(".backButt").click(function () {
+  current_commit = commits[parseInt($(".commitNum").html()) - 2]["commit_id"];
+  load_commit();
+});
+
+$(".loginButt").click(function () {
+  user_id = $(".login_view input").val();
+  if (user_id) {
+    load_user_commits();
+  } else {
+    alert("Champ manquant: Username.");
+  }
+});
+
+//Tangled
+$(".tangButt").click(function () {
+  $("._bjA").removeClass("d-none");
+  $(".com-code").addClass("clustering");
+  $("table tr, table td").removeClass("ui-selected")
+});
+
+//Atomique or Difficile à juger.
+$(".skipButt, .atomButt").click(function () {
+  $("._bjA").addClass("d-none");
+  $(".com-code").removeClass("clustering");
+});
+
+//Clic sur Valider
+$(".saveButt").click(function () {
+  if (save_check()) {
+      save_label();
+    }
+});
+
+//Verification before the validation
+function save_check() {
+  var flag = true;
+  errors = [];
+  activities = "";
+
+  if ($(".tangButt").hasClass("chosen")) { flag = clustering_check(); }
+  if (!$(".buttons button").hasClass("chosen")) {
+    flag = false;
+    errors.push("Sélectioner une option: Atomique, Enchevêtré ou Difficile à juger.");
+  }
+
+  if ($("#CheckPerfectif").is(":checked")) {
+    activities += "perfectif,";
+  }
+  if ($("#CheckCorrectif").is(":checked")) {
+    activities += "correctif,";
+  }
+  if ($("#CheckAdaptif").is(":checked")) {
+    activities += "adaptif";
+  }
+  if (activities.length == "") {
+    flag = false;
+    errors.push("Choisisser au moins un type: Perfectif, Adaptif ou Correctif.")
+  }
+  
+  $(".modal.errors ul").html("");
+  if (!flag) {
+    for (var e of errors) {
+      $(".modal.errors ul").append("<li>" + e + "</li>")
+    }
+    $(".modal.errors").modal("show");
+  }
+  return flag;
+}
+
+//Change cluster number
+$(".change td:first-child").click(function () {
+  var current = parseInt($(this).html());
+  $(this).html((current == 4)? 1 : (current+1))
+});
+
+$(".buttons button").click(function () {
+  $(".buttons button").removeClass("chosen");
+  $(this).addClass("chosen");
+});
+
+//init inputs with the default values
+function default_inputs(){
+  $('#CheckPerfectif, #CheckAdaptif, #CheckCorrectif').prop('checked', false);
+  $("._bjA input").val('');
+  $(".com-code").removeClass("clustering");
+  $("button").removeClass("chosen");
+  activities = "";
+  clusters = [];
+}
+
+//Tangled label verification
+function clustering_check() {
+  clusters = [];
+  $("._bjA > div").each(function (i) {
+    var cluster = { "number": (i + 1), "lines": "", "description": $(this).find("input").val() };
+    clusters.push(cluster);
+  });
+
+  $(".clustering .code").each(function (j) {
+    $(this).find("table tr").each(function (k) {
+      if (!$(this).hasClass("noChange")) {
+        var line = $(this).attr("data-line") + ",";
+        var cluster_num = $(this).find(".cluster_num").html();
+        if (cluster_num == "?") {
+          errors = ["Chaque ligne doit apparteninr à un cluster."]
+          return false;
+        }
+        var index = clusters.findIndex(x => x.number == cluster_num);
+        clusters[index]['lines'] += line;
+        
+      }
+    });
+  });
+  
+  var final_clusters = []
+  for (const [index, c] of clusters.entries()) {
+    if (c["lines"] == "" && c["description"] == "") {
+      continue;
+    } else {
+      if (c["lines"] && c["description"] == "") {
+        errors.push("Le cluster " + c["number"] + " nécessite une description.");
+      } else if (c["description"] && c["lines"] == "") {
+        errors.push("Le cluster " + c["number"] + " ne comprend aucune ligne.");
+      } 
+      final_clusters.push(c);  
+    }
+  }
+  if (final_clusters.length < 2) {
+    errors.push("Un commit enchevêtré doit contenir au moins 2 clusters.");
+  }
+  clusters = final_clusters;
+  return (errors.length)? false : true;
+}
+
+//Load a commit from Github
+function load_commit() {
+  fetch("http://localhost:3000/scraper/?commit_url=" + current_commit)
+    .then((response) => response.json())
+    .then((response) => {
+      var data = response["data"];
+      $(".project-name").html(data["project"]);
+      $(".message").html(data["message"]);
+
+      $("#temp").html(data["files"]);
+      //$(".js-expandable-line").remove();
+      $(".com-code").html("");
+      $(".file").each(function (i) {
+        var file = $(this);
+        var file_id = $(file).attr('id');
+        $(".com-code").append(file_template((i + 1) ,file_id));
+        $(".com-code .code[data-fileId=" + file_id + "] .fileName span:nth-child(2)").html($(file).find(".file-header").attr("data-path"));
+
+        $(file).find("tbody tr").each(function (j) {
+          var line = $(this);
+          if ($(line).attr("data-hunk")) {
+            var line_num = $(line).find(".js-linkable-line-number").attr("data-line-number");
+            var code = $(line).find(".blob-code-inner").html();
+            var type = "noChange";
+            if ($(line).find(".blob-num").hasClass("blob-num-addition")) {
+              type = "add";
+            } else if ($(line).find(".blob-num").hasClass("blob-num-deletion")) {
+              type = "del";
+            }
+            code.replace(/<[^>]+>/g, '');
+            $(".code[data-fileId=" + file_id + "] table").append(lines_template((i + 1), line_num, code, type));
+          }
+        });
+      });
+      $(".com-infos a").attr('href', current_commit)
+      $(".changes .selectable").selectable({
+        stop: function () {
+          var popup = $(this).closest(".code").find(".clusters_popup");
+          $(popup).addClass("d-none");
+          $(this).find("tr").each(function (j) {
+            if ($(this).hasClass("ui-selected") && !$(this).hasClass("noChange")) {
+              $(popup).removeClass("d-none");
+              return false;
+            }
+          });
+        }
+      });
+      $(".backButt").removeClass("d-none");
+      if (is_labled()) {
+        load_commit_labels();
+      } else {
+        default_inputs();
+      }
+    });
+}
+
+//Load a commit labels
+function load_commit_labels () {
+  fetch("http://localhost:3000/labels?commit_url=" + current_commit + "&user_id=" + user_id)
+    .then((response) => response.json())
+    .then((response) => {
+      var data = response["data"][0];
+      var index = commits.findIndex(x => x.commit_id == current_commit);
+      $(".commitNum").html(index + 1);
+      if (index == 0) { $(".backButt").addClass("d-none"); }
+      
+      default_inputs();
+
+      if (data["activities"].includes('perfectif')) {
+        $('#CheckPerfectif').prop('checked', true);
+      }
+      if (data["activities"].includes('adaptif')) {
+        $('#CheckAdaptif').prop('checked', true);
+      }
+      if (data["activities"].includes('correctif')) {
+        $('#CheckCorrectif').prop('checked', true);
+      }
+
+      var type = ".atomButt";
+      if (data["type"] == 'tangled') {
+        type = '.tangButt';
+        load_commit_clusters();
+      } else if (data["type"] == 'unknown') { type = '.skipButt'; }
+      $(type).trigger('click');
+    });
+}
+
+//Load a commit clusters
+function load_commit_clusters () {
+  fetch("http://localhost:3000/clusters?commit_url=" + current_commit + "&user_id=" + user_id)
+    .then((response) => response.json())
+    .then((response) => {
+      for (const cluster of response["data"]) {
+        var cluster_num = cluster["cluster"];
+        $("input[data-cluster=" + cluster_num + "]").val(cluster["description"]);
+        var lines = cluster["code_lines"].split(',')
+        for (const line of lines.slice(0, lines.length -1)) {
+          $("tr[data-line="+ line +"] .cluster_num").html(cluster_num);
+        }
+      }
+    });
+}
+
+//Return HTML file
+function file_template(index, file_id) {
+  var html_file = '<div class="row code" data-fileID="'+ file_id +'">' +
+                      '<div class="fileName"><span>File '+ index + ' >> </span><span></span></div>' +
+                      '<div class="changes">' +
+                        '<table><tbody class="selectable"></tbody></table>' +
+                      '</div>' +
+                      '<div class="clusters_popup d-none"><span>1</span><span>2</span><span>3</span><span>4</span></div>'
+                  '</div>';
+  return html_file;
+}
+
+//Return HTML code lines
+function lines_template(file_num, line_num, code, type) {
+  var sign = (type == 'add') ? ' +   ' : (type == 'del') ? ' -   ' : "     ";
+  var is_selectable = (type == "noChange") ? "" : "ui-widget-content";
+  return '<tr data-line="' + file_num + "-" + line_num + '" class=" ' + type + ' '+ is_selectable +'">' +
+            '<td class="cluster_num">?</td>' +
+            '<td>' + line_num + '</td>' +
+            '<td>' + sign + code +'</td>' +
+          '</tr>';
+}
+
+//Return commits that the user should label.
+function load_user_commits() {
+  fetch("http://localhost:3000/commits/"+ user_id)
+    .then((response) => response.json())
+    .then((data) => {
+      commits = data["data"];
+      if (commits.length) {
+        for (const [index, c] of commits.entries()) {
+          if (c["type"] == null) {
+            current_commit = c["commit_id"];
+            $(".commitNum").html(index + 1);
+            $(".TotalCom").html(commits.length)
+            break;
+          }
+        }
+        load_commit();
+        $(".login_view").addClass("d-none");
+        $(".mycontainer").removeClass("d-none");
+      } else {
+        alert("No data is available for this username.");
+      }
+    });
+}
+
+//Save a commit labels & clusters.
+function save_label() {
+  fetch("http://localhost:3000/save_label", {
+    method: "post",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      commit_id: current_commit,
+      user_id: user_id,
+      type: $("button.chosen").attr("data-type"),
+      activities: activities,
+      clusters: clusters
+    })
+  })
+  .then((response) => response.json())
+  .then((data) => {
+      commits_update();
+      var index = parseInt($(".commitNum").html());
+      if(index != parseInt($(".TotalCom").html())){
+        $(".commitNum").html(index + 1);
+        current_commit = commits[index]["commit_id"];
+        load_commit();
+      } else {
+        alert("END");
+      }
+   });
+}
+
+//Update commits list.
+function commits_update() {
+  var index = commits.findIndex(x => x.commit_id == current_commit);
+  commits[index]['type'] = $("button.chosen").attr("data-type");
+}
+
+//Check if a commit is unlabled yet
+function is_labled() {
+  var index = commits.findIndex(x => x.commit_id == current_commit);
+  return commits[index]["type"] != null;
+}
+
+//Add lines to a cluster
+$('.com-code').on('click', '.clusters_popup span', function () {
+  var value = $(this).html();
+  var lines = $(this).closest(".code").find("tbody tr");
+  $(lines).each(function (index) {
+    if ($(this).hasClass("ui-selected")) {
+      $(this).find(".cluster_num").html(value);
+    }
+  });
+  $(this).parent().addClass("d-none");
+  $(lines).removeClass("ui-selected")
+});
